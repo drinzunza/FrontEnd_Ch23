f2cbef2a440d54e0a109bbe55f029c588af6d109
Sergio Inzunza <sinzunza@sdgku.edu>
2021-11-14 12:47:29 -0700

    end of class 2

---
+++ services/dataService.js
@@ -0,0 +1,11 @@
+const data = [
+  {
+    _id: "1023lhk123kljglbmsdafd",
+    title: "Banana",
+    category: "Fruit",
+    image: "img24.png",
+    price: 12.23,
+    stock: 3,
+    minimum: 1,
+  },
+];
